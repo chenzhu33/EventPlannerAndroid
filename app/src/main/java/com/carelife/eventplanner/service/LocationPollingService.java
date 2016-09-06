57aebee1197827215df2fb54061f6c930d8fbc27
ChenZhuwei <czwcarelife@gmail.com>
2016-09-06 22:17:25 +0800

    Location

--- app/src/main/java/com/carelife/eventplanner/service/LocationPollingService.java
+++ app/src/main/java/com/carelife/eventplanner/service/LocationPollingService.java
@@ -7,8 +7,15 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+
+import java.util.List;
 
 /**
  * Created by chenzhuwei on 2016/9/6.
@@ -16,6 +23,8 @@ import android.os.IBinder;
 public class LocationPollingService extends Service {
 
     private NotificationManager mManager;
+    private LocationManager locationManager;
+    private String locationProvider;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -25,6 +34,24 @@ public class LocationPollingService extends Service {
     @Override
     public void onCreate() {
         mManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        //获取显示地理位置信息的TextView
+        //获取地理位置管理器
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //获取所有可用的位置提供器
+        List<String> providers = locationManager.getProviders(true);
+        if(providers.contains(LocationManager.GPS_PROVIDER)){
+            //如果是GPS
+            locationProvider = LocationManager.GPS_PROVIDER;
+        }else if(providers.contains(LocationManager.NETWORK_PROVIDER)){
+            //如果是Network
+            locationProvider = LocationManager.NETWORK_PROVIDER;
+        }else{
+            return ;
+        }
+
+        //监视地理位置变化
+        locationManager.requestLocationUpdates(locationProvider, 3000, 1, locationListener);
     }
 
     @Override
@@ -32,6 +59,34 @@ public class LocationPollingService extends Service {
         new PollingThread().start();
         return super.onStartCommand(intent, flag, startId);
     }
+
+    /**
+     * LocationListern监听器
+     * 参数：地理位置提供器、监听位置变化的时间间隔、位置变化的距离间隔、LocationListener监听器
+     */
+
+    LocationListener locationListener =  new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle arg2) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+    };
 
     //弹出Notification
     private void showNotification() {
